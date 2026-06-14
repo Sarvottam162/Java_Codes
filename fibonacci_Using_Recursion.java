5e112c28c4ace2f4f8d77e51e648ec331ec7e64b
Sarvottam162 <147574477+Sarvottam162@users.noreply.github.com>
2026-06-14 19:07:10 +0530

    Updated fibonacci program

--- fibonacci_Using_Recursion.java
+++ fibonacci_Using_Recursion.java
@@ -1,5 +1,14 @@
 public class fibonacci_Using_Recursion {
     public static void main(String[] args) {
-        
+        int n = 6 ; //nth fibonacci number is ? 0-based indexing
+        int nth= fibo(n);
+        System.out.println("nth fibonacci number is : "+nth);
+    }
+    public static int fibo(int n){
+        if( n == 0 || n == 1) return n;
+
+        int f1 = fibo(n - 1);
+        int f2 = fibo(n - 2);
+        return f1 + f2;
     }
 }
